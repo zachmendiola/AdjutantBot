a24be0d28d3fe39447f80e25e8c3ba11ae662e01
Zach Mendiola <zach.mendiola27@outlook.com>
2021-10-06 06:18:26 +0000

    Added new waifu features for rate limiting.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import time
 import requests
 import regex
 import re
+import schedule
+import time
 import messages
 import json
 import statements
@@ -21,6 +23,7 @@ db_name = 'defaultdb'
 bottoken = os.environ['bot_token']
 websterkey = os.environ['websterkey']
 client = discord.Client()
+seenwaifus = []
 
 @commands.command
 async def mention_ping(ctx, member : discord.Member):
@@ -30,10 +33,26 @@ async def mention_ping(ctx, member : discord.Member):
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
 
+async def dailycountreset():
+  print('Daily roll reset')
+  list = ['Chomper#4072','demie#2551','Captain Bulbarus#2248','Ascendance#5342','Ginchey#8598','Alpal Esq.#6166','baramz#7249','El Hobo#2195']
+  for i in list:
+    db[i] = 3
+
+dailycountreset()
+schedule.every(1).day.at("22:42").do(dailycountreset)
+
 #Event that monitors discord messages
 @client.event
 async def on_message(message):
   content = message.content.lower()
+
+  if content.startswith('$adminreset') and str(message.author) == 'Ascendance#5342':
+    list = ['Chomper#4072','demie#2551','Captain Bulbarus#2248','Ascendance#5342','Ginchey#8598','Alpal Esq.#6166','baramz#7249','El Hobo#2195']
+    for i in list:
+      db[i] = 3
+    await message.channel.send('Waifu rolls for the day have been manually reset.')
+
 
   if message.author == client.user:
       return
@@ -43,7 +62,7 @@ async def on_message(message):
 
   if content.startswith('$experts'):
     await messages.experts(message)
-
+    
   if content.startswith('$lookup'):
     try:
       words = content.split(' ')
@@ -63,14 +82,44 @@ async def on_message(message):
 
   if content.startswith('$waifu'):
     categories = ['waifu','neko','shinobu','megumin','bully','cuddle','cry','hug',
-      'lick','pat','smug','bonk','yeet','blush','smile','wave','nom','bite','slap',
-      'kill','kick','happy','wink','poke','dance','cringe']
-    choice = random.choice(categories)
-    response = requests.get('https://api.waifu.pics/sfw/'+choice)
+    'lick','pat','smug','bonk','yeet','blush','smile','wave','nom','bite','slap',
+    'kill','kick','happy','wink','poke','dance','cringe']
+    types = ['sfw','nsfw']
+
+    if '-type' in message.content:
+      split = content.split(' ')
+      idx = split.index('-type')
+      typewaifu = split[idx+1]
+      if typewaifu not in types:
+        await message.channel.send("Sorry, the type you've selected is invalid. Please choose from: sfw, nsfw")
+    else: 
+      typewaifu = 'sfw'
+    
+    if '-category' in message.content:
+      split = content.split(' ')
+      idx = split.index('-category')
+      categorywaifu = split[idx+1]
+      if categorywaifu not in categories:
+        await message.channel.send("Sorry, the category you've selected is invalid. Please choose from: waifu, neko, shinobu, megumin, bully, cuddle, cry, hug, lick, pat, smug, bonk, yeet, blush, smile, wave, nom, bite, slap, kill, kick,happy, wink, poke, dance or cringe.")
+    else:
+      if typewaifu == 'nsfw':
+        nsfwcategories = ['waifu','neko']
+        categorywaifu = random.choice(nsfwcategories)
+      else:
+        categorywaifu = random.choice(categories)
+
+    response = requests.get('https://api.waifu.pics/'+typewaifu+'/'+categorywaifu)
     dict = response.json()
-    await message.channel.send(dict['url'])
-    print(message.author.name+'-'+choice)
-    db['waifu'] += 1
+    db['latest'] = dict['url']
+    print(message.author.name+'-'+categorywaifu)
+    if db[str(message.author)] == 0:
+      await message.channel.send('Sorry, you\'ve run out of waifu rolls for the day. :(')
+      return
+    else:
+      db[str(message.author)] -= 1
+      await message.channel.send(dict['url'])
+      await message.channel.send('You have '+str(db[str(message.author)])+' waifu rolls left for the day.')
+      return
     
   if content.startswith('$dog'):
     response = requests.get('https://dog.ceo/api/breeds/image/random')
@@ -134,16 +183,23 @@ async def on_message(message):
     await message.channel.send(';;play https://www.youtube.com/watch?v=04FfDVZ1O14&ab_channel=SKwon')
   
   if content.startswith('$inspire'):
-    response = requests.get('https://api.fisenko.net/v1/quotes/en/random')
+    response = requests.get('https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand')
     dict = response.json()
-    quote = dict['text']+' - '+dict['author']['name']
+    quote = dict['quote']+' - '+dict['author']
     await message.channel.send(quote)
   
   if content.startswith('$joke'):
     response = requests.get('https://icanhazdadjoke.com/slack')
     dict = response.json()
     await message.channel.send(dict['attachments'][0]['text'])
-
+  
+  if content.startswith('$bis'):
+    db[str(message.author)+'bis'] = db['latest']
+    await message.channel.send(message.author.name+' got BiS!\n''Saved favorite for: '+message.author.name)
+    print('Saved favorite: '+ message.author.name)
+  
+  if content.startswith('$showbis'):
+    await message.channel.send(db[str(message.author)+'bis'])
 
 keep_alive()
 client.run(bottoken)
